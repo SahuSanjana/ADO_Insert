2996cdee994d1076aed5668f660f76864fe39313
SahuSanjana <sanjanasahu0112@gmail.com>
2023-11-02 10:48:59 +0530

    Add project files.

---
+++ Ado/StudentInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO_Insert.Ado
+{
+    class StudentInformation
+    {
+        public void InsertDataIntoStudent_Info(int Roll_number, string Student_name, long Contact_number)
+        {
+            try
+            {
+                string cn = ConfigurationManager.ConnectionStrings["MyServer"].ConnectionString;
+                SqlConnection myCn = new SqlConnection(cn);
+                myCn.Open();
+                string query = "StdProInsertIntoStudent_Info";
+                SqlCommand cmd = new SqlCommand(query, myCn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Roll_number", Roll_number);
+                cmd.Parameters.AddWithValue("@Student_name", Student_name);
+                cmd.Parameters.AddWithValue("@Contact_number", Contact_number);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using ADO_Insert.Ado;
+namespace ADO_Insert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInformation information = new StudentInformation();
+            Console.WriteLine("Please enter your Roll number: ");
+            int rNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your name: ");
+            string sName = Console.ReadLine();
+            Console.WriteLine("Please enter your phone number: ");
+            long cNumber = Convert.ToInt64(Console.ReadLine());
+            information.InsertDataIntoStudent_Info(rNumber, sName, cNumber);
+        }
+       
+    }
+}
